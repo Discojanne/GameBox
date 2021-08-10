81f58b87d62a3ff6615f63855e167b05a4fd7d14
DAVID BENGTSSON <db96@live.se>
2021-08-10 21:35:33 +0200

    test

---
+++ GameBox/GameBox/ECS/Systems/CollisionSystem.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "entityx/entityx.h"
+#include "SFML/Graphics/Sprite.hpp"
+
+
+class CollisionSystem : public entityx::System<CollisionSystem>
+{
+public:
+	CollisionSystem();
+	~CollisionSystem();
+
+	void update(entityx::EntityManager& es, entityx::EventManager& events, entityx::TimeDelta dt) override;
+
+private:
+
+};
